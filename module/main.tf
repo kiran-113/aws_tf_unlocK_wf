f4356784fe91d0f247efec4e6ab0887ab631e4b9
cloudopsdojo <cloudopsdojo@gmail.com>
2024-10-18 14:50:16 +0530

    version upgraded

--- module/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_s3_bucket" "this" {
-  for_each      = toset(var.s3_bucket_name)#-{random_string.random_number.id}
-  bucket        = each.value
-  force_destroy = var.force_destroy
-}
